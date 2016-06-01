fca7f495e38eb4bcdab332a21b4b9225872e2fb5
Flowdeeps <speedwolf@gmail.com>
2016-06-02 00:17:34 +0100

    First pass of pretty much the entire thing. Most of it is CSS3 animations and transforms, the rest is just positional data and a trigger provided by js.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,3 +1,31 @@
 window.onload = function() {
-	
+
+	var notThere = document.getElementsByClassName( 'not-there' ),
+			here = document.getElementById( 'no_here' ),
+			trigger = document.getElementById( 'trigger' ),
+			i = 0;
+
+
+	// the fantastic getOffset function was stolen lock stock and barrel from http://stackoverflow.com/a/442474
+	function getOffset( el ) {
+	    var _x = 0;
+	    var _y = 0;
+	    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+	        _x += el.offsetLeft - el.scrollLeft;
+	        _y += el.offsetTop - el.scrollTop;
+	        el = el.offsetParent;
+	    }
+	    return { top: _y, left: _x };
+	}
+
+	trigger.onclick = function( event ) {
+		event.preventDefault();
+		if ( i < ( notThere.length - 1 ) ) {
+			i++;
+		} else {
+			i = 0;
+		}
+		here.setAttribute( 'style', "transform: translate(" + getOffset( notThere[ i ] ).left + "px, "+ getOffset( notThere[ i ] ).top +"px); transition: transform 1s ease-in-out;");
+	}
+
 };
