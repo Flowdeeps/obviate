42a83acc51ffc695e9a89ada89d48cbe1d3a460e
Pete Haughie <pete.haughie@sourcefabric.org>
2016-06-02 14:08:51 +0100

    Some refactoring. Now if you want to search for strings in the page it parses the entire body as a string, adds <blink> tags around the matches and then quirts the showle thing back into the body innerHTML. I know. It's gross and should probably be illegal but it works and it's here now. Why blink tags? Because nobody else is using them so it can't break any modern website.

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,35 +1,64 @@
 window.onload = function() {
 
-	var notThere = document.getElementsByClassName( 'not-there' ),
-			here = document.getElementById( 'no_here' ),
-			trigger = document.getElementById( 'trigger' ),
-			i = 0;
+	var setUp = function() {
 
+		var body = document.getElementsByTagName( 'body' );
+
+		var notThereOrig = body[0].innerHTML,
+				matchMe = "Lorem";
+				matchRegEx = /Lorem/gi;
+
+		var notThereNew = notThereOrig.replace( matchRegEx, "<blink>" + matchMe + "</blink>" );
+
+		var reinsert = function(callback) {
+			body[0].innerHTML = notThereNew;
+			ready;
+		}();
+
+		var ready = function(callback) {
+
+			var here = document.getElementById( 'no_here' ),
+					trigger = document.getElementById( 'trigger' ),
+					i = 0;
+
+			var notThere = document.getElementsByTagName( 'blink' );
+			// var notThere = document.getElementsByClassName( 'not-there' );
+
+			var next = function() {
+
+				if ( i < ( notThere.length - 1 ) ) {
+					i++;
+				} else {
+					i = 0;
+				}
+
+				here.setAttribute( 'style', "transform: translate(" + getOffset( notThere[ i ] ).left + "px, "+ getOffset( notThere[ i ] ).top +"px); transition: transform 1s ease-in-out;");
+
+			}
+
+			trigger.onclick = function( event ) {
+				event.preventDefault();
+				next();
+			}
+
+		}();
+
+	}();
 
 	// the fantastic getOffset function was stolen lock stock and barrel from http://stackoverflow.com/a/442474
 	function getOffset( el ) {
-	    var _x = 0;
-	    var _y = 0;
-	    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-	        _x += el.offsetLeft - el.scrollLeft;
-	        _y += el.offsetTop - el.scrollTop;
-	        el = el.offsetParent;
-	    }
-	    return { top: _y, left: _x };
-	}
 
-	var next = function() {
-		if ( i < ( notThere.length - 1 ) ) {
-			i++;
-		} else {
-			i = 0;
-		}
-		here.setAttribute( 'style', "transform: translate(" + getOffset( notThere[ i ] ).left + "px, "+ getOffset( notThere[ i ] ).top +"px); transition: transform 1s ease-in-out;");		
-	}
+		var _x = 0,
+    		_y = 0;
 
-	trigger.onclick = function( event ) {
-		event.preventDefault();
-		next();
+		while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+	    _x += el.offsetLeft - el.scrollLeft;
+      _y += el.offsetTop - el.scrollTop;
+      el = el.offsetParent;
+    }
+
+		return { top: _y, left: _x };
+
 	}
 
 };
